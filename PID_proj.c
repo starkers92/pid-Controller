01450e59241b3c9693631af8af072a7e904865cc
starkers92 <starkers92@users.noreply.github.com>
2018-08-27 09:15:27 +1000

    Update PID_proj.c

--- PID_proj.c
+++ PID_proj.c
@@ -1,65 +1,126 @@
-/*
- * PID_proj.c
- *
- *  Created on: 27 Aug. 2018
- *      Author: Dave
- */
 
+#include "pid.h"
 
-/*
- ============================================================================
- Name        : PID_proj.c
- Author      : Chris
- Version     :
- Copyright   : Free as in beer
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+void PID_Reset(struct s_PID *s) {
+	s->Error=0;
+	s->Error_Last=0;
+	s->Integral=0;
+}
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "pid/pid.h"
+void PID_Init(struct s_PID *s) {
+	s->Reference=0;
+	s->Measured=0;
+	s->Output=0;
 
-#define myKp 1
-#define myKi 1
-#define myKd 1
-#define myTs 1
-#define myMin 1
-#define myMax 1
-#define myRef 1
+	s->Error=0;
+	s->Error_Last=0;
+	s->Integral=0;
+	s->Derivative=0;
 
+	s->Kp=0;
+	s->Ki=0;
+	s->Kd=0;
+	s->Ts=0;
 
-int main(void) {
+	s->Actuator_Max=0;
+	s->Actuator_Min=0;
 
-	/*
-	 * Declare struct for PID controller to hold variables
-	 * And another to hold the return values & status
-	 */
-	struct s_PID MyPIDController;
-	struct s_PIDOutput MyPIDControllerOutput;
-	/*
-	 * -Initiate variables to zero
-	 * -Set gains
-	 * -Set actuator limits
-	 */
-	PID_Init(&MyPIDController);
-	PID_SetGains(&MyPIDController, myKp, myKi, myKd, myTs);
-	PID_SetReference(&MyPIDController, myRef);
-	PID_SetActuatorLimits(&MyPIDController, myMin, myMax);
-	/*
-	 *
-	 */
-	while(1) {
-		//Wait for timer flag..
+	s->e_SaturatedStatus = PID_NotSaturated;
+}
 
-		//Calculate PID
-		MyPIDControllerOutput = PID_Calculate(&MyPIDController);
+void PID_SetReference(struct s_PID *s, ControlTypeDef ref) {
+	s->Reference=ref;
+}
 
-		if(MyPIDControllerOutput.e_OutputSaturatedStatus != PID_NotSaturated) {
-			//code to run on saturation
-		}
+void PID_SetGains(struct s_PID *s, ControlTypeDef Kp, ControlTypeDef Ki, ControlTypeDef Kd, ControlTypeDef Ts) {
+	s->Kp=Kp;
+	s->Ki=Ki;
+	s->Kd=Kd;
+	s->Ts=Ts;
+}
 
-		//Send MyPIDControllerOutput.Output to actuator function..
+void PID_SetActuatorLimits(struct s_PID *s, ControlTypeDef Min, ControlTypeDef Max) {
+	s->Actuator_Max=Max;
+	s->Actuator_Max=Min;
+}
+
+struct s_PIDOutput PID_Calculate(struct s_PID *s) {
+	s->Error = s->Reference - s->Measured; //Error = Reference - Measured
+
+	s->Integral = s->Integral + (s->Error)*(s->Ts); //Integral = Integral + Error*Ts
+
+	s->Derivative = (s->Error - s->Error_Last)/(s->Ts); //Derivative = (Error-Error_Last)/Ts
+
+	s->Error_Last = s->Error; //Error_Last = Error
+
+	s->Saturation_Test = (s->Kp)*(s->Error) + (s->Ki)*(s->Integral) + (s->Kd)*(s->Derivative); 	//Saturation_Test = Kp * Error + Ki*Integral
+
+	if( ( (s->Saturation_Test) <= s->Actuator_Min ) ||  ((s->Saturation_Test) >= s->Actuator_Max ) ) { //If Sat_Test < Min OR Sat_test > Max 
+		//Anti-windup mechanism
+		s->Integral = s->Integral - (s->Error) * (s->Ts); //Integral = Integral - Error*Ts;
+		s->Output = s->Saturation_Test - (s->Ki)*(s->Integral);
 	}
-	return EXIT_SUCCESS;
+	else {
+	//Actually do the PI
+	//Output = Kp*error + Ki*integral
+	s->Output = s->Saturation_Test;
+	}
+	//Check for actuator saturation
+	if( (s->Output) <= (s->Actuator_Min) ) {
+		(s->Output) = (s->Actuator_Min); //Apply saturation limit
+		s->e_SaturatedStatus = PID_SaturatedMin; //Set Saturation Flag
+	}
+	else if ( (s->Output) >= (s->Actuator_Max) ) {
+		(s->Output) = (s->Actuator_Max); //Apply saturation limit
+		s->e_SaturatedStatus = PID_SaturatedMax; //Set Saturation Flag
+	}
+	else {
+		s->e_SaturatedStatus = PID_NotSaturated; //Set Saturation Flag
+	}
+	
+	struct s_PIDOutput s_Output = {
+			s->Output,
+			s->e_SaturatedStatus,
+	};
+	return s_Output;
+}
+
+
+struct s_PIDOutput PI_Calculate(struct s_PID *s) {
+	s->Error = s->Reference - s->Measured; //Error = Reference - Measured
+
+	s->Integral = s->Integral + (s->Error)*(s->Ts); //Integral = Integral + Error*Ts
+
+	s->Error_Last = s->Error; //Error_Last = Error
+
+	s->Saturation_Test = (s->Kp)*(s->Error) + (s->Ki)*(s->Integral); 	//Saturation_Test = Kp * Error + Ki*Integral
+
+	if( ( (s->Saturation_Test) <= s->Actuator_Min ) ||  ((s->Saturation_Test) >= s->Actuator_Max ) ) { //If Sat_Test < Min OR Sat_test > Max
+		//Anti-windup mechanism
+		s->Integral = s->Integral - (s->Error) * (s->Ts); //Integral = Integral - Error*Ts;
+		s->Output = s->Saturation_Test - (s->Ki)*(s->Integral);
+	}
+
+	//Actually do the PI
+	//Output = Kp*error + Ki*integral
+	s->Output = s->Saturation_Test;
+
+	//Check for actuator saturation
+	if( (s->Output) <= (s->Actuator_Min) ) {
+		(s->Output) = (s->Actuator_Min); //Apply saturation limit
+		s->e_SaturatedStatus = PID_SaturatedMin; //Set Saturation Flag
+	}
+	else if ( (s->Output) >= (s->Actuator_Max) ) {
+		(s->Output) = (s->Actuator_Max); //Apply saturation limit
+		s->e_SaturatedStatus = PID_SaturatedMax; //Set Saturation Flag
+	}
+	else {
+		s->e_SaturatedStatus = PID_NotSaturated; //Set Saturation Flag
+	}
+
+	struct s_PIDOutput s_Output = {
+			s->Output,
+			s->e_SaturatedStatus,
+	};
+	return s_Output;
 }
